5c086afc6d84424376a8d30ca50e53112afb72f4
NetOpsTechRon <johndoe@example.com>
2026-02-24 08:37:36 -0500

    MG and Flamers 2500 Base Leader BV

--- MekHQ/src/mekhq/campaign/stratCon/StratConRulesManager.java
+++ MekHQ/src/mekhq/campaign/stratCon/StratConRulesManager.java
@@ -133,7 +133,7 @@ import org.apache.commons.math3.util.Pair;
  */
 public class StratConRulesManager {
     private static final String RESOURCE_BUNDLE = "mekhq.resources.StratConRulesManager";
-    public final static int BASE_LEADERSHIP_BUDGET = 1000;
+    public final static int BASE_LEADERSHIP_BUDGET = 2500;
 
     private static final MMLogger LOGGER = MMLogger.create(StratConRulesManager.class);
 
